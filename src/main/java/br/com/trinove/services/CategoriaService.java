9c7693ec64cc9dd9802cf96282fe069570920b6f
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-07 15:19:54 -0300

    Validacao sintatica com Bean validation

---
+++ src/main/java/br/com/trinove/services/CategoriaService.java
@@ -0,0 +1,68 @@
+package br.com.trinove.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.trinove.domain.Categoria;
+import br.com.trinove.dto.CategoriaDTO;
+import br.com.trinove.repositories.CategoriaRepository;
+import br.com.trinove.services.exception.DateIntegrityException;
+import br.com.trinove.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id){
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não e possível excluir categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+
+}
