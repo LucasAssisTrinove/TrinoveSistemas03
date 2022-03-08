1ee7e2676397c3191bc3d2a92c86bb0e53289b2c
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-08 16:06:18 -0300

    Busca de produtos por nome e categorias

--- src/main/java/br/com/trinove/repositories/ProdutoRepository.java
+++ src/main/java/br/com/trinove/repositories/ProdutoRepository.java
@@ -1,11 +1,24 @@
 package br.com.trinove.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.trinove.domain.Categoria;
 import br.com.trinove.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	//@Transactional(readOnly=true)
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj obj.nome LIKE %:nome% END cat IN :categorias")
+	//Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	@Transactional(readOnly = true )
+	Page<Produto> findDistincByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
 }
