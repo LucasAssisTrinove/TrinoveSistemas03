4eaabfd912500d9d52288c47adbf78a3fa3620b1
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-08 08:31:40 -0300

    Inserindo um novo cliente com POST

--- src/main/java/br/com/trinove/resouces/ClienteResource.java
+++ src/main/java/br/com/trinove/resouces/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.trinove.resouces;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.trinove.domain.Cliente;
-import br.com.trinove.domain.Cliente;
 import br.com.trinove.dto.ClienteDTO;
+import br.com.trinove.dto.ClienteNewDTO;
 import br.com.trinove.services.ClienteService;
 
 
@@ -72,6 +74,14 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 
--- src/main/java/br/com/trinove/services/ClienteService.java
+++ src/main/java/br/com/trinove/services/ClienteService.java
@@ -10,10 +10,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.trinove.domain.Cidade;
 import br.com.trinove.domain.Cliente;
+import br.com.trinove.domain.Endereco;
+import br.com.trinove.domain.enums.TipoCliente;
 //import br.com.trinove.domain.Cliente;
 import br.com.trinove.dto.ClienteDTO;
+import br.com.trinove.dto.ClienteNewDTO;
+import br.com.trinove.repositories.CidadeRepository;
 import br.com.trinove.repositories.ClienteRepository;
+import br.com.trinove.repositories.EnderecoRepository;
 import br.com.trinove.services.exception.DateIntegrityException;
 import br.com.trinove.services.exception.ObjectNotFoundException;
 
@@ -24,6 +30,12 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 
 	
 	public Cliente find(Integer id) {
@@ -31,6 +43,13 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -60,6 +79,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfouCnpj(), TipoCliente.toEnum( objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
