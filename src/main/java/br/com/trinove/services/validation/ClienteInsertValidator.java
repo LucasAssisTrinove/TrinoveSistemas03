9a3fb362f9d4c7a4eec4ed26689c3acfc6cab88d
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-08 10:32:35 -0300

    Validacao custumizada: email nao repetido na insercao de cliente

--- src/main/java/br/com/trinove/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/trinove/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.trinove.domain.Cliente;
 import br.com.trinove.domain.enums.TipoCliente;
 import br.com.trinove.dto.ClienteNewDTO;
+import br.com.trinove.repositories.ClienteRepository;
 import br.com.trinove.resouces.exception.FieldMessage;
 import br.com.trinove.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfouCnpj())) {
 			list.add(new  FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
