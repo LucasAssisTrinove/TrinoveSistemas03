3e022680b7a3c9c3171dba1a68094cab387eedbb
Lucas Assis Lima <lucas.lima@trinove.com.br>
2022-03-09 17:12:13 -0300

    Salvando perfis de usuários na base de dados

--- src/main/java/br/com/trinove/domain/enums/Perfil.java
+++ src/main/java/br/com/trinove/domain/enums/Perfil.java
@@ -1,14 +1,14 @@
 package br.com.trinove.domain.enums;
 
-public enum TipoCliente {
+public enum Perfil {
 	
-	PESSOAFISICA(1, "Pessoa Física"),
-	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	ADMIN(1, "ROLE_ADMIN"),
+	CLIENTE(2, "ROLE_CLIENTE");
 	
 	private int cod;
 	private String descricao;
 	
-	private TipoCliente(int cod, String descricao) {
+	private Perfil(int cod, String descricao) {
 		this.cod = cod;
 		this.descricao = descricao;
 		
@@ -22,12 +22,12 @@ public enum TipoCliente {
 		return descricao;
 	}
 	
-	public static TipoCliente toEnum(Integer cod) {
+	public static Perfil toEnum(Integer cod) {
 		if (cod == null) {
 			return null;
 			}
 		
-		for (TipoCliente x : TipoCliente.values()) {
+		for (Perfil x : Perfil.values()) {
 			
 			if(cod.equals(x.getCod())) {
 				return x;
@@ -36,5 +36,6 @@ public enum TipoCliente {
 		throw new IllegalArgumentException("Id inválido: "+ cod);
 		
 	}
+	
 
 }
--- src/main/java/br/com/trinove/services/DBService.java
+++ src/main/java/br/com/trinove/services/DBService.java
@@ -20,6 +20,7 @@ import br.com.trinove.domain.PagamentoComCartao;
 import br.com.trinove.domain.Pedido;
 import br.com.trinove.domain.Produto;
 import br.com.trinove.domain.enums.EstadoPagamento;
+import br.com.trinove.domain.enums.Perfil;
 import br.com.trinove.domain.enums.TipoCliente;
 import br.com.trinove.repositories.CategoriaRepository;
 import br.com.trinove.repositories.CidadeRepository;
@@ -124,17 +125,22 @@ public class DBService {
 		
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Lucas Assis", "lucas.assis@trinove.com.br", "64898513697", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Florriano", "2106", null, "Centro", "38777012", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
